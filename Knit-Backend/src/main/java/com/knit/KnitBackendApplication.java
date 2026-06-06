bb4f7a5de0df9372afb2aa57715dcef25845e4d9
Darsh_Balar <darshbalar2005@gmail.com>
2026-06-06 09:23:08 +0530

    first commit

---
+++ Knit-Backend/src/main/java/com/knit/KnitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.knit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnitBackendApplication.class, args);
+	}
+
+}
